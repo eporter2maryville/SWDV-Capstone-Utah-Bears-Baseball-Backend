d7a27340a12aa2186c73e905ba7f7ca7299caa75
eporter2maryville <39113621+eporter2maryville@users.noreply.github.com>
2020-04-27 05:19:46 -0600

    Populate Announcements Table

---
+++ SQL Statements/populate_Announcements.sql
@@ -0,0 +1,10 @@
+INSERT INTO "announcements" ("AnnouncementNumber", "Email", "SessionID"
+, "EventNumber", "Date", "Body") 
+VALUES ('1', NULL, NULL
+, NULL, '2020-02-29', 'The Season Schedule has been announced and will be emailed soon!')
+, ('2', NULL, NULL, 
+NULL, '2020-03-13', 'The Season has officially been put on hold due to Corona Virus. 
+Please be advised the schedule is at risk.')
+, ('3', NULL, NULL, 
+NULL, '2020-04-23', 'Rules have been approved by Salt Lake County for play to begin during 
+phase 1 of the relaxation in Corona Virus response. Please watch for additional details.')
